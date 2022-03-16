b4df66b799d1004232071c0c16bdf39b63f98bf6
João Paulo Gomes <johnowl@users.noreply.github.com>
2022-03-16 11:52:33 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,6 +76,8 @@ publishing {
             artifactId = "owl-rules"
             version = version
             from(components["java"])
+            artifact(javadocJar)
+            artifact(sourcesJar)
 
             pom {
                 name.set("Owl Rules")
